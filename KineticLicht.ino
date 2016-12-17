64be293327a22d6f6e865ae133934523bc5194a3
gmoehler <greggmoeh@gmx-topmail.de>
2016-12-17 08:16:15 +0100

    correct accelstepper stuff

--- KineticLicht.ino
+++ KineticLicht.ino
@@ -8,13 +8,15 @@
 
 
 // two stepper motors one on each port
-Adafruit_MotorShield AFMS_a(0x60); 
-Adafruit_MotorShield AFMS_b(0x61); 
-
+Adafruit_MotorShield AFMS_a = Adafruit_MotorShield(0x60); 
+Adafruit_MotorShield AFMS_b = Adafruit_MotorShield(0x61); 
+/*
 Adafruit_StepperMotor *motor1 = AFMS_a.getStepper(200, 1);
 Adafruit_StepperMotor *motor2 = AFMS_a.getStepper(200, 2);
 Adafruit_StepperMotor *motor3 = AFMS_b.getStepper(200, 1);
 Adafruit_StepperMotor *motor4 = AFMS_b.getStepper(200, 2);
+*/
+
 
 #define data   4
 #define clock  5
@@ -85,11 +87,17 @@ KeyFrameRgb rgb4u_kf[] {
   {7000, BLUE}
 };
 
-
+/*
 KeyFrameStepper  stepper1 = KeyFrameStepper(motor1, 1, motor1_kf, 2);
 KeyFrameStepper  stepper2 = KeyFrameStepper(motor2, 2, motor2_kf, 2);
 KeyFrameStepper  stepper3 = KeyFrameStepper(motor3, 3, motor3_kf, 2);
 KeyFrameStepper  stepper4 = KeyFrameStepper(motor4, 4, motor4_kf, 2);
+*/
+Adafruit_StepperMotor *stepper1 = AFMS_a.getStepper(200, 1);
+Adafruit_StepperMotor *stepper2 = AFMS_a.getStepper(200, 2);
+Adafruit_StepperMotor *stepper3 = AFMS_b.getStepper(200, 1);
+Adafruit_StepperMotor *stepper4 = AFMS_b.getStepper(200, 2);
+
 KeyFrameRgbLED      rgb1o = KeyFrameRgbLED (&tlc, 0, rgb1u_kf, 3);
 KeyFrameRgbLED      rgb1u = KeyFrameRgbLED (&tlc, 1, rgb1o_kf, 3);
 KeyFrameRgbLED      rgb2o = KeyFrameRgbLED (&tlc, 2, rgb2u_kf, 3);
@@ -98,40 +106,69 @@ KeyFrameRgbLED      rgb3o = KeyFrameRgbLED (&tlc, 4, rgb3u_kf, 3);
 KeyFrameRgbLED      rgb3u = KeyFrameRgbLED (&tlc, 5, rgb3o_kf, 3);
 KeyFrameRgbLED      rgb4o = KeyFrameRgbLED (&tlc, 6, rgb4u_kf, 3);
 KeyFrameRgbLED      rgb4u = KeyFrameRgbLED (&tlc, 7, rgb4o_kf, 3);
+// you can change these to DOUBLE or INTERLEAVE or MICROSTEP!
+void forwardstep1() {  
+  stepper1->onestep(FORWARD, DOUBLE);
+}
+void backwardstep1() {  
+  stepper1->onestep(BACKWARD, DOUBLE);
+}
+AccelStepper astepper1(forwardstep1, backwardstep1); // use functions to step
+
+void forwardstep2() {  
+  stepper2->onestep(FORWARD, DOUBLE);
+}
+void backwardstep2() {  
+  stepper2->onestep(BACKWARD, DOUBLE);
+}
+
+AccelStepper astepper2(forwardstep2, backwardstep2); // use functions to step
+
+void forwardstep3() {  
+  stepper3->onestep(FORWARD, DOUBLE);
+}
+void backwardstep3() {  
+  stepper3->onestep(BACKWARD, DOUBLE);
+}
+
+AccelStepper astepper3(forwardstep3, backwardstep3); // use functions to step
+
+void forwardstep4() {  
+  stepper4->onestep(FORWARD, DOUBLE);
+}
+void backwardstep4() {  
+  stepper4->onestep(BACKWARD, DOUBLE);
+}
+
+AccelStepper astepper4(forwardstep4, backwardstep4); // use functions to step
 
 RgbLED led = RgbLED( &tlc, 4);
-
-AccelStepper astepper1(motor1->onestep(FORWARD, SINGLE), motor1->onestep(BACKWARD, SINGLE)); // use functions to step
-AccelStepper astepper2(motor2->onestep(FORWARD, SINGLE), motor2->onestep(BACKWARD, SINGLE)); // use functions to step
-AccelStepper astepper3(motor3->onestep(FORWARD, SINGLE), motor3->onestep(BACKWARD, SINGLE)); // use functions to step
-AccelStepper astepper4(motor4->onestep(FORWARD, SINGLE), motor4->onestep(BACKWARD, SINGLE)); // use functions to step
-
 
 void setup()
 {
   Serial.begin(9600);
   Serial.println("start");
-  
+
   AFMS_a.begin();
   AFMS_b.begin();
-  
+/*  
   stepper1.start();
   stepper2.start();
   stepper3.start();
   stepper4.start();
-
+*/
   astepper1.setSpeed(50);
   astepper2.setSpeed(100);
   astepper3.setSpeed(200);
-  astepper4.setSpeed(50);
-
+  astepper4.setSpeed(4000);
+/*
   astepper1.moveTo(300);
   astepper2.moveTo(500);
   astepper3.moveTo(200);
   astepper4.moveTo(300);
-
+*/
   tlc.begin();
-  led.doFineSerialOutput(true);
+  //led.doFineSerialOutput(true);
   
   rgb1o.start();
   rgb1u.start();
@@ -150,11 +187,11 @@ void loop()
  //stepper3.loop();
  //stepper4.loop();
 
-  astepper1.runSpeed();
+  //astepper1.runSpeed();
   astepper2.runSpeed();
-  astepper3.runSpeed();
+  //astepper3.runSpeed();
   astepper4.runSpeed();
-
+ 
   //rgb1o.loop();
   //rgb1u.loop();
   //rgb2o.loop();
