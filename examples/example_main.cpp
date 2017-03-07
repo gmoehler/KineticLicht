db1cff0ff8b90af5dfa96bcb64c9d655562a5f10
Gregor Moehler <gmoeh@gmx.de>
2017-03-07 19:01:13 +0100

    redo initialization

--- examples/example_main.cpp
+++ examples/example_main.cpp
@@ -2,50 +2,92 @@
 
 void test1(){
 
-	unsigned t1[][7] = {
-		{LED1BOT, 0, 0, BLACK, 0},
+
+ 
+	int t1[][5] = {
+		{0, 0, 0, 0, 0},
 	};
 
-	unsigned t2[][7] = {
-		{LED1BOT, 0, 0, BLACK, 0},
-		{LED1TOP, 0, 0, BLACK, 0},
+	unsigned t2[][5] = {
+		{0, 0, 0, 0, 0},
+		{1, 1, 1 ,1 , 1},
 	};
 
-	unsigned t3[][7] = {
-		{LED1BOT, 0, 0, BLACK, 0},
-		{LED1TOP, 0, 0, BLACK, 0},
-		{LED2BOT, 0, 0, BLACK, 0},
+	unsigned t3[][5] = {
+		{0, 0, 0, 0, 0},
+		{1, 1, 1 ,1, 1},
+		{2, 2, 2, 2, 2},
 	};
+	
+	
+	
 
 	unsigned ***allt = 0;
 	allt = new unsigned**[3];
 
 	int rows[3] = {1,2,3};
 
-	//int rows1 = sizeof(t1) / sizeof(t1[0][0]);
-  auto t_heap1 = new unsigned[rows[0]][7]();
-	std::copy(&t1[0][0], &t1[0][0]+rows[0]*7,&t_heap1[0][0]);
-	allt[0] = (unsigned**) t_heap1;
+int sizeX = 5;
+int sizeZ = 3;
 
-	//int rows2 = sizeof(t2) / sizeof(t2[0][0]);
-  auto t_heap2 = new unsigned[rows[1]][7]();
-	std::copy(&t2[0][0], &t2[0][0]+rows[1]*7,&t_heap2[0][0]);
-	allt[1] = (unsigned**) t_heap2;
+int ***ary = new int**[sizeZ];
+for(int i= 0;  i< sizeZ; ++i) {
+  ary[i] = new int*[rows[i]];
+  for(int j= 0; j < rows[i]; ++j) {
+    ary[i][j] = new int[sizeX];
+    for(int k=0; k< sizeX; ++k) {
+    	ary[i][j][k] = 99;
+        switch (i){
+        	case 0:
+              ary[i][j][k] = t1[j][k];
+              break;
+            case 1:
+              ary[i][j][k] = t2[j][k];
+              break;
+            case 2:
+              ary[i][j][k] = t3[j][k];
+              break;
+        }
+    }
+  }
+}
 
-	//int rows3 = sizeof(t3) / sizeof(t3[0][0]);
-	auto t_heap3 = new unsigned[rows[2]][7]();
-	std::copy(&t3[0][0], &t3[0][0]+rows[2]*7,&t_heap3[0][0]);
-	allt[2] = (unsigned**) t_heap3;
-
+printf("hallo\n");
 	for (int i=0; i<3; i++){
 		for (int j=0; j<rows[i]; j++){
-		 for (int k=0; k<7; k++){
+		 for (int k=0; k<5; k++){
+			 printf("%d", ary[i][j][k]);
+		 }
+		 printf ("\n");
+		}
+	}
+/*
+
+	//int rows1 = sizeof(t1) / sizeof(t1[0][0]);
+    auto t_heap1= new int [rows[0]][5]();
+	std::copy(&t1[0][0], &t1[0][0]+rows[0]*5,&t_heap1[0][0]);
+	allt[0] = (int**) t_heap1;
+
+	//int rows2 = sizeof(t2) / sizeof(t2[0][0]);
+  auto t_heap2 = new int[rows[1]][5]();
+	std::copy(&t2[0][0], &t2[0][0]+rows[1]*5,&t_heap2[0][0]);
+	allt[1] = (int**) t_heap2;
+
+	//int rows3 = sizeof(t3) / sizeof(t3[0][0]);
+	auto t_heap3 = new int[rows[2]][5]();
+	std::copy(&t3[0][0], &t3[0][0]+rows[2]*5,&t_heap3[0][0]);
+	allt[2] = (int**) t_heap3;
+
+    printf("hallo\n");
+	for (int i=0; i<3; i++){
+		for (int j=0; j<rows[i]; j++){
+		 for (int k=0; k<5; k++){
 			 printf("%d", allt[i][j][k]);
 		 }
 		 printf ("\n");
 		}
 	}
-
+*/
 }
 
 
