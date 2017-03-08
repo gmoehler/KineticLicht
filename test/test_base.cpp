f2da8f4b701f277faa03cbdb1717715ed2dc91fb
Gregor Moehler <gregor.moehler@gmx.de>
2017-03-08 18:23:07 +0100

    add removed function

--- test/test_base.cpp
+++ test/test_base.cpp
@@ -1,7 +1,7 @@
 #include "test.h"
 
 void RGB_EXPECT_EQ (int a, int b){
-	
+
 	if (a == RGB_MAX_VAL){
 		EXPECT_EQ(a,b);
 	}
@@ -9,5 +9,11 @@ void RGB_EXPECT_EQ (int a, int b){
 		int a0 = a / RGB_COMPRESSION * RGB_COMPRESSION;
 		EXPECT_EQ(a0, b);
 	}
-	
+
+}
+
+void expect_rgb(RGB rgb, int r, int g, int b){
+  RGB_EXPECT_EQ(r, rgb.red());
+  RGB_EXPECT_EQ(g, rgb.green());
+  RGB_EXPECT_EQ(b, rgb.blue());
 }
